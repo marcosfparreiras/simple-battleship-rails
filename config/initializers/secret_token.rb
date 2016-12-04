73e2b7091b05a5f47ff067a11c82c7ddf67c01be
marcosfparreiras <marcosfparreiras@gmail.com>
2016-12-04 16:40:16 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleBattleshipRails::Application.config.secret_key_base = 'db24c9037f37440c4d6dfb1f6b0f61e19b2eba35ee0548729730b831cf1afc456ff9f7b245f6bab00d868463eb78c5cf1498d3b4d15f8b2d7ae510a69af5c205'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleBattleshipRails::Application.config.session_store :cookie_store, key: '_simple-battleship-rails_session'
