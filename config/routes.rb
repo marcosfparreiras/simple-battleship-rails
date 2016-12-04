55a6b9f230686c5a04c3ab40d272859da7ddef46
marcosfparreiras <marcosfparreiras@gmail.com>
2016-12-04 16:43:06 -0200

    Create static page for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SimpleBattleshipRails::Application.routes.draw do
+  root "static_pages#home"
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ SimpleBattleshipRails::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
